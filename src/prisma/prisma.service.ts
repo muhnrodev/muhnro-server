f836c71e85aac7d497917bbd9b7eb3fb33633ac8
Dev Admin <dev.admin@muhnro.com>
2026-07-17 20:09:28 -0400

    fix: update winston config log stream name and enhance Prisma service with SSL support

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import * as fs from 'fs';
 
 @Global()
 @Injectable()
@@ -15,7 +16,13 @@ export class PrismaService
 {
   constructor() {
     const connectionString = process.env.DATABASE_URL || '';
-    const adapter = new PrismaPg({ connectionString });
+
+    const adapter = new PrismaPg({
+      connectionString,
+      ssl: {
+        ca: fs.readFileSync('certs/rds-ca.pem').toString(),
+      },
+    });
     super({ adapter });
   }
 
